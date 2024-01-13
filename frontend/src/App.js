06b761f83b5efae53967494a0d0ca2c671b8b3fe
crunchydosa123 <prathamgadkari@gmail.com>
2024-01-13 19:49:16 +0530

    basic issues with tailwind sorted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='bg-gray-200'>Hello</h1>
+      <Navbar />
+      <h1 className='bg-blue-600'>Hello</h1>
     </div>
   );
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../index.css'
+
+const Navbar = () => {
+  return (
+    <div className='bg-blue-300'>
+        <h1>Blogs by Pratham</h1>
+    </div>
+  )
+}
+
+export default Navbar

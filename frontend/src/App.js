68f125a7b5e5f03f7dcaa0ade6a4a0c2b6736681
crunchydosa123 <prathamgadkari@gmail.com>
2024-01-23 15:09:42 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
-import './App.css';
-import Homepage from './pages/Homepage';
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Loginpage from './pages/Loginpage';
-import Signuppage from './pages/Signuppage';
-import NotFoundpage from './pages/NotFoundpage';
-import SingleBlog from './pages/SingleBlog';
-import Dashboard from './pages/Dashboard';
+  import './App.css';
+  import Homepage from './pages/Homepage';
+  import { Routes, Route, BrowserRouter } from "react-router-dom";
+  import Loginpage from './pages/Loginpage';
+  import Signuppage from './pages/Signuppage';
+  import NotFoundpage from './pages/NotFoundpage';
+  import SingleBlog from './pages/SingleBlog';
+  import Dashboard from './pages/Dashboard';
 
-function App() {
-  return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={ <Homepage /> } />
-      <Route path="/login" element={ <Loginpage /> } />
-      <Route path="/signup" element={ <Signuppage /> } />
-      <Route path="/single" element={ <SingleBlog /> } />
-      <Route path="/dashboard" element={ <Dashboard /> } />
-      <Route path="*" element={ <NotFoundpage /> } />
-    </Routes>
-    </BrowserRouter>
+  function App() {
+    return (
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Homepage /> } />
+        <Route path="/login" element={ <Loginpage /> } />
+        <Route path="/signup" element={ <Signuppage /> } />
+        <Route path="/single" element={ <SingleBlog /> } />
+        <Route path="/dashboard" element={ <Dashboard /> } />
+        <Route path="*" element={ <NotFoundpage /> } />
+      </Routes>
+      </BrowserRouter>
 
-  );
-}
+    );
+  }
 
-export default App;
+  export default App;
